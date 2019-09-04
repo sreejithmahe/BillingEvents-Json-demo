655735a1a87b93c2aa8756d064ccaa96a8162975
sreejithmahe <sreejith.mahe@gmail.com>
2019-09-05 00:00:36 +0530

    Testing Goalseek

--- src/main/java/org/rbs/nordisk/invoice/Goalseek.java
+++ src/main/java/org/rbs/nordisk/invoice/Goalseek.java
@@ -7,7 +7,7 @@ public class Goalseek {
 	}
 
 	static int Result(int a,int b,int c,int r) {
-		r = 2+(b*c);
+		r = a+(b*c);
 		return r;
 	}
 	
@@ -16,10 +16,27 @@ public class Goalseek {
 		return Goalseek;
 	}
 	
+	static double LandingPrice (double BasePrice,double TransportCharge) {
+		double SalesTax =  BasePrice*(4.5/100);
+		double Octroi =    BasePrice*(3.0/100);
+		double Insurance=  (BasePrice+SalesTax)*0.07/100;
+		System.out.println(BasePrice + "  -  " + SalesTax + "   -   " +Octroi + "   -  " +Insurance + "  -  " +TransportCharge + "   -  "  );
+		return BasePrice+SalesTax+Octroi+Insurance+TransportCharge;
+	}
+	
+	static double LandingPrice (double SalesTax,double Octroi,double Insurance, double TransportCharge,double LandingPrice) {
+		
+		return LandingPrice-(SalesTax+Octroi+Insurance+TransportCharge);
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 	System.out.println(	Result(2,4,2,0));
 	System.out.println(	Result(4,2,10));
+	System.out.println(	LandingPrice(1389.8,5.0));
+	System.out.println(	LandingPrice(62.5,41.7,1.0,5.0,1500));
 
 	}
 
